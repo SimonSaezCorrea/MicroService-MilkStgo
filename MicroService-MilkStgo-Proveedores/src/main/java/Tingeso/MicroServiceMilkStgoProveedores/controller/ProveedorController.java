4966802bf657a38203e1e1379f6eaae72a31bc1b
Simon Saez <nayxxaxx789super@gmail.com>
2023-06-10 00:22:58 -0400

    Proveedor

---
+++ MicroService-MilkStgo-Proveedores/src/main/java/Tingeso/MicroServiceMilkStgoProveedores/controller/ProveedorController.java
@@ -0,0 +1,38 @@
+package Tingeso.MicroServiceMilkStgoProveedores.controller;
+
+import Tingeso.MicroServiceMilkStgoProveedores.entity.ProveedorEntity;
+import Tingeso.MicroServiceMilkStgoProveedores.service.ProveedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/proveedor")
+public class ProveedorController {
+
+    @Autowired
+    private ProveedorService proveedorService;
+
+    @GetMapping()
+    public ResponseEntity<List<ProveedorEntity>> listar() {
+        List<ProveedorEntity> proveedores = proveedorService.obtenerProveedores();
+        if(proveedores.isEmpty())
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(proveedores);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProveedorEntity> proveedor(@PathVariable("id") String id){
+        ProveedorEntity proveedor = proveedorService.encontrarPorCodigo(id);
+        if(proveedor == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(proveedor);
+    }
+    @PostMapping()
+    public ResponseEntity<ProveedorEntity> nuevoProveedor(@RequestBody ProveedorEntity proveedor){
+        ProveedorEntity proveedorNew = proveedorService.guardarProveedor(proveedor);
+        return ResponseEntity.ok(proveedorNew);
+    }
+}
---
+++ MicroService-MilkStgo-Proveedores/src/main/java/Tingeso/MicroServiceMilkStgoProveedores/repository/ProveedorRepository.java
@@ -0,0 +1,19 @@
+package Tingeso.MicroServiceMilkStgoProveedores.repository;
+
+import Tingeso.MicroServiceMilkStgoProveedores.entity.ProveedorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProveedorRepository extends JpaRepository<ProveedorEntity, String>{
+    @Query("select e from ProveedorEntity e where e.nombre = :nombre")
+    ProveedorEntity encontrarPorNombre(@Param("nombre") String nombre);
+
+    @Query("select e.categoria from ProveedorEntity e where e.proveedor_id = :proveedor_id")
+    String encontrarCategoria(@Param("proveedor_id") String codigo);
+
+    @Query("select e from ProveedorEntity e where e.proveedor_id = :proveedor_id")
+    ProveedorEntity encontrarPorCodigo(@Param("proveedor_id")String codigo);
+}
